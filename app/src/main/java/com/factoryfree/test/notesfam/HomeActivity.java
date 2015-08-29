8b85c2016f6b24ae06f30c06d8cf8cddab328a1a
bastien <bastien.guichard@gmail.com>
2015-08-29 22:06:17 +0200

    base facebook sdk

--- app/src/main/java/com/factoryfree/test/notesfam/HomeActivity.java
+++ app/src/main/java/com/factoryfree/test/notesfam/HomeActivity.java
@@ -1,15 +1,27 @@
 package com.factoryfree.test.notesfam;
-import android.app.Fragment;
+import android.app.AlertDialog;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.FragmentActivity;
 import android.view.Menu;
 import android.view.MenuItem;
+
+import com.facebook.CallbackManager;
+import com.facebook.FacebookAuthorizationException;
+import com.facebook.FacebookCallback;
+import com.facebook.FacebookException;
 import com.facebook.FacebookSdk;
+import com.facebook.ProfileTracker;
+import com.facebook.appevents.AppEventsLogger;
+import com.facebook.login.LoginManager;
+import com.facebook.login.LoginResult;
 
 public class HomeActivity extends FragmentActivity {
 
-    // instance variable for your MainFragment class
-    private MainFragment mainFragment;
+
+    private CallbackManager callbackManager;
+    private ProfileTracker profileTracker;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,14 +31,45 @@ public class HomeActivity extends FragmentActivity {
         // especially, if you're using Facebook UI elements.
         /////////////////////////////////////
         FacebookSdk.sdkInitialize(this.getApplicationContext());
-        if (savedInstanceState == null){
-            mainFragment = new MainFragment();
-            getSupportFragmentManager().beginTransaction().add(android.R.id.content, mainFragment).commit();
-        }
-        else{
-            // Or set the fragment from restored state info
-            mainFragment = (MainFragment) getSupportFragmentManager().findFragmentById(android.R.id.content);
-        }
+        callbackManager = CallbackManager.Factory.create();
+
+        LoginManager.getInstance().registerCallback(callbackManager, new FacebookCallback<LoginResult>() {
+            @Override
+            public void onSuccess(LoginResult loginResult) {
+                handlePendingAction();
+                updateUI();
+            }
+
+            @Override
+            public void onCancel() {
+                if (pendingAction != PendingAction.NONE) {
+                    showAlert();
+                    pendingAction = PendingAction.NONE;
+                }
+                updateUI();
+            }
+
+            @Override
+            public void onError(FacebookException e) {
+                if (pendingAction != PendingAction.NONE
+                        && exception instanceof FacebookAuthorizationException) {
+                    showAlert();
+                    pendingAction = PendingAction.NONE;
+                }
+                updateUI();
+            }
+
+            private void showAlert() {
+                new AlertDialog.Builder(HomeActivity.this)
+                        .setTitle(R.string.cancelled)
+                        .setMessage(R.string.permission_not_granted)
+                        .setPositiveButton(R.string.ok, null)
+                        .show();
+            }
+        });
+
+
+        setContentView(R.layout.main);
 
     }
 
@@ -51,4 +94,74 @@ public class HomeActivity extends FragmentActivity {
 
         return super.onOptionsItemSelected(item);
     }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+
+        // Call the 'activateApp' method to log an app event for use in analytics and advertising
+        // reporting.  Do so in the onResume methods of the primary Activities that an app may be
+        // launched into.
+        AppEventsLogger.activateApp(this);
+
+        updateUI();
+    }
+
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        callbackManager.onActivityResult(requestCode, resultCode, data);
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+
+        // Call the 'deactivateApp' method to log an app event for use in analytics and advertising
+        // reporting.  Do so in the onPause methods of the primary Activities that an app may be
+        // launched into.
+        AppEventsLogger.deactivateApp(this);
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        profileTracker.stopTracking();
+    }
+
+    private void updateUI(){
+
+        
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 }
