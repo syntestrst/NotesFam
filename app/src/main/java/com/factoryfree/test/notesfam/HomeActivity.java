7d21f49c7abd198139302471fd76a7b109c033d4
bastien <bastien.guichard@gmail.com>
2015-08-30 15:39:37 +0200

    clean shareCallback onActivityResult manifest aplication: home, Login, Splash

--- app/src/main/java/com/factoryfree/test/notesfam/HomeActivity.java
+++ app/src/main/java/com/factoryfree/test/notesfam/HomeActivity.java
@@ -1,10 +1,16 @@
 package com.factoryfree.test.notesfam;
 
 import android.app.Activity;
-import android.content.Context;
 import android.os.Bundle;
 import android.widget.TextView;
-import android.widget.Toast;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+
 
 /**
  * Created by test on 30/08/2015.
@@ -12,16 +18,12 @@ import android.widget.Toast;
 
 public class HomeActivity extends Activity{
 
-
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        Context context = getApplicationContext();
-        CharSequence text = "Hello Homeactivity!";
-        int duration = Toast.LENGTH_SHORT;
-        Toast toast = Toast.makeText(context, text, duration);
-        toast.show();
         setContentView(R.layout.activity_home);
     }
+
+
+
 }
--- app/src/main/java/com/factoryfree/test/notesfam/LoginActivity.java
+++ app/src/main/java/com/factoryfree/test/notesfam/LoginActivity.java
@@ -3,68 +3,33 @@ import android.app.Activity;
 import android.app.AlertDialog;
 import android.content.Intent;
 import android.os.Bundle;
-import android.support.v4.app.FragmentActivity;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 
-
+import com.facebook.AccessToken;
 import com.facebook.CallbackManager;
 import com.facebook.FacebookAuthorizationException;
 import com.facebook.FacebookCallback;
 import com.facebook.FacebookException;
 import com.facebook.FacebookSdk;
-import com.facebook.Profile;
 import com.facebook.login.LoginManager;
 import com.facebook.login.LoginResult;
-import com.facebook.share.Sharer;
-import com.facebook.share.widget.ShareDialog;
 
+/**
+ * Created by test on 29/08/2015.
+ */
 
 public class LoginActivity extends Activity {
 
 
 
     private CallbackManager callbackManager;
-    private ShareDialog shareDialog;
-    private FacebookCallback<Sharer.Result> shareCallback = new FacebookCallback<Sharer.Result>() {
-        @Override
-        public void onCancel() {
-            Log.d("HelloFacebook", "Canceled");
-        }
-
-        @Override
-        public void onError(FacebookException error) {
-            Log.d("HelloFacebook", String.format("Error: %s", error.toString()));
-            String title = getString(R.string.error);
-            String alertMessage = error.getMessage();
-            showResult(title, alertMessage);
-        }
-
-        @Override
-        public void onSuccess(Sharer.Result result) {
-            Log.d("HelloFacebook", "Success!");
-            if (result.getPostId() != null) {
-                String title = getString(R.string.success);
-                String id = result.getPostId();
-                String alertMessage = getString(R.string.successfully_posted_post, id);
-                showResult(title, alertMessage);
-            }
-        }
-
-        private void showResult(String title, String alertMessage) {
-            new AlertDialog.Builder(LoginActivity.this)
-                    .setTitle(title)
-                    .setMessage(alertMessage)
-                    .setPositiveButton(R.string.ok, null)
-                    .show();
-        }
-
-    };
-
+    private AccessToken accessToken;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        accessToken = AccessToken.getCurrentAccessToken();
         //////////////////////////////////////////////
         // Initialize the SDK before executing any other operations,
         // especially, if you're using Facebook UI elements.
@@ -83,18 +48,17 @@ public class LoginActivity extends Activity {
 
             @Override
             public void onSuccess(LoginResult loginResult) {
-                updateUI();
+                Log.d("onSuccess", "onSuccess");
 
             }
 
             @Override
             public void onCancel() {
-                System.out.println("onCancel");
+                Log.d("onCancel", "onCancel");
             }
 
             @Override
             public void onError(FacebookException exception) {
-                System.out.println("onCancel");
                 if (exception instanceof FacebookAuthorizationException) {
                     showAlert();
                 }
@@ -108,10 +72,6 @@ public class LoginActivity extends Activity {
                         .show();
             }
         });
-        shareDialog = new ShareDialog(this);
-        shareDialog.registerCallback(
-                callbackManager,
-                shareCallback);
 
         setContentView(R.layout.activity_login);
 
@@ -139,27 +99,17 @@ public class LoginActivity extends Activity {
         return super.onOptionsItemSelected(item);
     }
 
-
-    private void updateUI(){
-        Profile profile = Profile.getCurrentProfile();
-        if (profile != null) {
-
-        } else {
-
-        }
-
-    }
-
     ////////////////////////////////////////////////////////
     // The CallbackManager manages the callbacks into the FacebookSdk
     // from an Activity's or Fragment's onActivityResult() method.
     //////////////////////////////////////////////////
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        Log.d("onActivityResult","onActivityResult");
         super.onActivityResult(requestCode, resultCode, data);
         callbackManager.onActivityResult(requestCode, resultCode, data);
-        Intent intentHomeActivity = new Intent(LoginActivity.this, HomeActivity.class);
-        startActivity(intentHomeActivity);
+        Intent intent = new Intent(this, HomeActivity.class);
+        startActivity(intent);
         finish();
     }
 
