59105f63d8f960669a75ab985538b095ec4e40d7
bastien <bastien.guichard@gmail.com>
2015-09-04 14:59:39 +0200

    convert image to bytearray => ok install sdk parse => ok upload image to class at parse.com =>ok extract name file from URI and use for upload in server => ok

--- app/src/main/java/com/factoryfree/test/notesfam/HomeActivity.java
+++ app/src/main/java/com/factoryfree/test/notesfam/HomeActivity.java
@@ -2,7 +2,9 @@ package com.factoryfree.test.notesfam;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.database.Cursor;
 import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.net.Uri;
 import android.os.Bundle;
 import android.os.Environment;
@@ -10,9 +12,16 @@ import android.provider.MediaStore;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
-import android.widget.TextView;
 import android.widget.Toast;
+
+import com.parse.Parse;
+import com.parse.ParseFile;
+import com.parse.ParseObject;
+
+import java.io.ByteArrayOutputStream;
 import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 
@@ -23,7 +32,7 @@ import java.util.Date;
 
 public class HomeActivity extends Activity{
 
-
+    private static final String TAG = HomeActivity.class.getSimpleName(); // for TAG Log class
     private static final int CAPTURE_IMAGE_ACTIVITY_REQUEST_CODE = 100; // onActivityResult call request code
     public static final int MEDIA_TYPE_IMAGE = 1;
     private Uri fileUri;
@@ -32,6 +41,8 @@ public class HomeActivity extends Activity{
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        Parse.initialize(this); // init SDK Parse
         setContentView(R.layout.activity_home);
         Button button = (Button)findViewById(R.id.button);
         button.setOnClickListener(mListenner);
@@ -52,15 +63,13 @@ public class HomeActivity extends Activity{
             // You must provide a fully qualified file name where the camera app should save the photo.
             ///////////////////
             fileUri = getOutputMediaFileUri(MEDIA_TYPE_IMAGE); // create a file to save the image
-
+            intent.putExtra(MediaStore.EXTRA_OUTPUT, fileUri); // set the image file name
             //////////////////////////////////////
             // small Bitmap in the extras, under the key "data".
             // Bundle extras = data.getExtras();
             // Bitmap imageBitmap = (Bitmap) extras.get("data");
             // mImageView.setImageBitmap(imageBitmap);
             // ///////////////
-            /*intent.putExtra(MediaStore.EXTRA_OUTPUT, fileUri); // set the image file name*/
-
 
             ////////////////////////////////////
             // Start camera Intent (Implicit MediaStore.ACTION_IMAGE_CAPTURE)
@@ -81,26 +90,48 @@ public class HomeActivity extends Activity{
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         if (requestCode == CAPTURE_IMAGE_ACTIVITY_REQUEST_CODE) {
             if (resultCode == RESULT_OK) {
-                Toast.makeText(this, "Image saved to:\n" + fileUri, Toast.LENGTH_LONG).show();
-                Log.d("FileUri","result " + fileUri);
+                /*Toast.makeText(this, "Image saved to:\n" + fileUri, Toast.LENGTH_LONG).show();*/
+                Log.d(TAG, "result " + fileUri);
+                try{
 
-                if(fileuri not null){
-                    // Encode crypt and upload at class on parse.com
-                    if(upload call back ok){
-                        // delete on Environment.getExternalStoragePublicDirectory
+                    ///////////////////////////////////////
+                    // image to byteArray
+                    //
+                    ////////////////
+                    Bitmap bitmap = MediaStore.Images.Media.getBitmap(this.getContentResolver(), fileUri);
+                    ByteArrayOutputStream stream = new ByteArrayOutputStream();
+                    bitmap.compress(Bitmap.CompressFormat.PNG, 100, stream);
+                    byte[] byteArray = stream.toByteArray();
+
+                    ///////////////////////////////////////
+                    // SDK parse upload at class
+                    //
+                    ///////////////
+                    String fileName = null;
+                    String scheme = fileUri.getScheme();
+                    if (scheme.equals("file")) {
+                        fileName = fileUri.getLastPathSegment();
                     }
-                    else{
-                        // Error upload on parse.com class
-                    }
+
+                    Log.d(TAG, "result " + fileName);
+                    ParseFile file = new ParseFile(fileName, byteArray);
+                    file.saveInBackground();
+                    ParseObject TestObject = new ParseObject("LivePic");
+                    TestObject.put("livepic", file);
+                    TestObject.saveInBackground();
                 }
-                else{
-                    // Error fileuri
+                catch(FileNotFoundException e){
+                    e.printStackTrace();
                 }
+                catch(IOException e){
+                    e.printStackTrace();
+                }
+
             } else if (resultCode == RESULT_CANCELED) {
                 // User cancelled the image capture
             } else {
                 // Image capture failed, advise user
-                Log.d("cas2", "here");
+                Log.d(TAG, "here");
             }
         }
     }
@@ -140,4 +171,5 @@ public class HomeActivity extends Activity{
 
         return mediaFile;
     }
+
 }
