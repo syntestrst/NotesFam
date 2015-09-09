d0e1e422f0c2c43025d295eed589146c4a65d9f8
bastien <bastien.guichard@gmail.com>
2015-09-09 19:48:58 +0200

    implements fragments need read fucking doc. inject fragments for menu drawer = ok resultonactivity on fragment = ok listenner onclick with fragment  = ok inject submenu = ok

--- app/src/main/java/com/factoryfree/test/notesfam/FirstFragment.java
+++ app/src/main/java/com/factoryfree/test/notesfam/FirstFragment.java
@@ -1,9 +1,243 @@
 package com.factoryfree.test.notesfam;
 
-import android.support.v4.app.FragmentActivity;
+import android.app.Activity;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.net.Uri;
+import android.os.Bundle;
+import android.os.Environment;
+import android.provider.MediaStore;
+import android.support.v4.app.Fragment;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.LinearLayout;
+
+import com.parse.Parse;
+import com.parse.ParseFile;
+import com.parse.ParseObject;
+import com.parse.ProgressCallback;
+import com.parse.SaveCallback;
+
+import java.io.ByteArrayOutputStream;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 /**
  * Created by test on 07/09/2015.
  */
-public class FirstFragment extends FragmentActivity{
+public class FirstFragment extends Fragment implements View.OnClickListener {
+
+    private static final String TAG = HomeActivity.class.getSimpleName(); // for TAG Log class
+    private static final int CAPTURE_IMAGE_ACTIVITY_REQUEST_CODE = 100; // onActivityResult call request code
+    private static final int MEDIA_TYPE_IMAGE = 1;
+
+    public Uri fileUri;
+    public File mediaFile;
+    private String fileName = null;
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        Parse.initialize(this.getActivity()); // init SDK Parse*/
+
+
+        LinearLayout mLinearLayout = (LinearLayout) inflater.inflate(R.layout.first_fragment,
+                container, false);
+        Button button = (Button) mLinearLayout.findViewById(R.id.button);
+        button.setOnClickListener(mListenner);
+        /*return inflater.inflate(R.layout.first_fragment, container, false);*/
+        return mLinearLayout;
+    }
+    private View.OnClickListener mListenner = new View.OnClickListener() {
+        @Override
+        public void onClick(View view) {
+            ////////////////////////////////////
+            // Compose Intent camera
+            ////////////////////////////
+            Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+
+            ///////////////////////////////////////
+            //The Android Camera application saves a full-size photo if you give it a file to save into.
+            // You must provide a fully qualified file name where the camera app should save the photo.
+            ///////////////////
+            fileUri = getOutputMediaFileUri(MEDIA_TYPE_IMAGE); // create a file to save the image
+            intent.putExtra(MediaStore.EXTRA_OUTPUT, fileUri); // set the image file name
+
+            //////////////////////////////////////
+            // small Bitmap in the extras, under the key "data".
+            // Bundle extras = data.getExtras();
+            // Bitmap imageBitmap = (Bitmap) extras.get("data");
+            // mImageView.setImageBitmap(imageBitmap);
+            // ///////////////
+
+            ////////////////////////////////////
+            // Start camera Intent (Implicit MediaStore.ACTION_IMAGE_CAPTURE)
+            ////////////////////////////
+            startActivityForResult(intent, CAPTURE_IMAGE_ACTIVITY_REQUEST_CODE);
+        }
+    };
+
+
+    /** Create a file Uri for saving an image or video */
+    private Uri getOutputMediaFileUri(int type){
+        return Uri.fromFile(getOutputMediaFile(type));
+    }
+
+    /** Create a File for saving an image or video */
+    private File getOutputMediaFile(int type){
+
+        // To be safe, you should check that the SDCard is mounted
+        // using Environment.getExternalStorageState() before doing this.
+        if(isExternalStorageMounted()){
+
+            // This location works best if you want the created images to be shared
+            // between applications and persist after your app has been uninstalled.
+            File mediaStorageDir = new File(Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_PICTURES), "MyCameraApp");
+
+            // getFilesDir() Returns a File representing an internal directory for your app.
+            // To create a new file in one of these directories, you can use the File() constructor.
+            // internal IS ============== PRIVATE FOR OTHER APP ==================
+            /*Context context = this;
+            File mediaStorageDir = new File(context.getFilesDir(), "LivePic");
+            Log.d(TAG, "result_fileuri=" + fileUri);*/
+
+            // Create the storage directory if it does not exist
+            if (! mediaStorageDir.exists()){
+                if (! mediaStorageDir.mkdirs()){
+                    Log.d("MyCameraApp", "failed to create directory");
+                    return null;
+                }
+            }
+            // Create a media file name
+            String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
+
+            if (type == MEDIA_TYPE_IMAGE){
+                mediaFile = new File(mediaStorageDir.getPath() + File.separator + "IMG_"+ timeStamp + ".jpg");
+            } else {
+                return null;
+            }
+            return mediaFile;
+        }
+        else if(isExternalStorageMountedOncomputer()){
+            ////////////////////////////////////
+            // Error with Environment.getExternalStorageState()
+            // media is connected to computer.
+            // i want block.
+            //////////////////////////
+            Log.e("MountedAtcomputer", "external storage is connected at computer");
+            return null;
+        }
+        else{
+            // Error with Environment.getExternalStorageState()
+            // media is not available.
+            Log.e("StorageMounted", "external storage not available");
+            return null;
+        }
+
+    }
+
+    /** Checks if external storage is available for read and write */
+    private boolean isExternalStorageMounted(){
+        String state = Environment.getExternalStorageState();
+        if(Environment.MEDIA_MOUNTED.equals(state)){
+            return true;
+        }
+        return false;
+    }
+
+    /** Checks if external storage the media is being shared (connected to a computer)
+     *
+     * */
+    private boolean isExternalStorageMountedOncomputer(){
+        String state = Environment.getExternalStorageState();
+        if(Environment.MEDIA_SHARED.equals(state)){
+            return true;
+        }
+        return false;
+    }
+
+    @Override
+    public void onClick(View v) {
+
+    }
+
+    ////////////////////////////////////
+    // Receive a camera Intent Result
+    // A result code specified by the second activity.
+    // This is either RESULT_OK if the operation was successful
+    // or RESULT_CANCELED if the user backed out or the operation failed for some reason.
+    ////////////////////////////
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == 100) {
+            if (resultCode == Activity.RESULT_OK) {
+                /*Toast.makeText(this, "Image saved to:\n" + fileUri, Toast.LENGTH_LONG).show();*/
+                Log.d("onActivityResult", "result " + fileUri);
+                try{
+
+                    ///////////////////////////////////////
+                    // image to byteArray
+                    // First, you'll need to have the data in byte[] form and then create a ParseFile with it.
+                    ////////////////
+                    Bitmap bitmap = MediaStore.Images.Media.getBitmap(getActivity().getContentResolver(), fileUri);
+                    ByteArrayOutputStream stream = new ByteArrayOutputStream(); //Creates a new byte array output stream. The buffer capacity is initially 32 bytes, though its size increases if necessary.
+                    bitmap.compress(Bitmap.CompressFormat.JPEG, 100, stream);
+                    byte[] byteArray = stream.toByteArray(); // Creates a newly allocated byte array.
+                    stream.close();
+
+                    // URI to String for name file in server
+                    String scheme = fileUri.getScheme();
+                    if (scheme.equals("file")) {
+                        fileName = fileUri.getLastPathSegment();
+                    }
+                    ParseFile file = new ParseFile(fileName, byteArray);
+
+                    // ///////////////////////////////////////
+                    // As with ParseObject, there are many variants of the save method you can use depending
+                    // on what sort of callback and error handling suits you.
+                    ///////////////
+                    file.saveInBackground(new SaveCallback() {
+                        @Override
+                        public void done(com.parse.ParseException e) {
+                            // Handle success or failure here ...
+                            mediaFile.delete();
+                        }
+                    }, new ProgressCallback() {
+
+                        public void done(Integer percentDone) {
+                            // Update your progress spinner here. percentDone will be between 0 and 100.
+                        }
+                    });
+
+                    ///////////////////////////////////////
+                    // Finally, after the save completes, you can associate a
+                    // ParseFile onto a ParseObject just like any other piece of data
+                    ///////////////
+                    ParseObject Parseobject = new ParseObject("LivePic");
+                    Parseobject.put("livepic", file);
+                    /*Parseobject.pin("updateAt", new Date());*/
+                    Parseobject.saveInBackground();
+                }
+                catch(FileNotFoundException e){
+                    e.printStackTrace();
+                }
+                catch(IOException e){
+                    e.printStackTrace();
+                }
+
+            } else if (resultCode == Activity.RESULT_CANCELED) {
+                // User cancelled the image capture
+            } else {
+                // Image capture failed, advise user
+                Log.d(TAG, "here");
+            }
+        }
+    }
 }
--- app/src/main/java/com/factoryfree/test/notesfam/HomeActivity.java
+++ app/src/main/java/com/factoryfree/test/notesfam/HomeActivity.java
@@ -1,41 +1,15 @@
 package com.factoryfree.test.notesfam;
 
-import android.app.Fragment;
-import android.app.FragmentManager;
-import android.content.Intent;
 import android.content.res.Configuration;
-import android.graphics.Bitmap;
-import android.net.Uri;
 import android.os.Bundle;
-import android.os.Environment;
-import android.provider.MediaStore;
 import android.support.design.widget.NavigationView;
-import android.support.v4.app.FragmentActivity;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.util.Log;
 import android.view.MenuItem;
-import android.view.View;
-import android.widget.Button;
-import android.widget.ListView;
-
-import com.parse.Parse;
-import com.parse.ParseFile;
-import com.parse.ParseObject;
-import com.parse.ProgressCallback;
-import com.parse.SaveCallback;
-
-import java.io.ByteArrayOutputStream;
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.io.IOException;
-import java.text.SimpleDateFormat;
-import java.util.Date;
-
 
 /**
  * Created by test on 30/08/2015.
@@ -49,59 +23,54 @@ public class HomeActivity extends AppCompatActivity{
      */
 
 
-    private static final String TAG = HomeActivity.class.getSimpleName(); // for TAG Log class
-    private static final int CAPTURE_IMAGE_ACTIVITY_REQUEST_CODE = 100; // onActivityResult call request code
-    private static final int MEDIA_TYPE_IMAGE = 1;
-    private Uri fileUri;
-    private String fileName = null;
-    private File mediaFile;
-
-
     private DrawerLayout mDrawer;
-    private Toolbar toolbar;
-    private NavigationView nvDrawer;
-    private DrawerLayout dlDrawer;
-    private ActionBarDrawerToggle drawerToggle;
+    private Toolbar mToolbar;
+    private NavigationView mnvDrawer;
+    private ActionBarDrawerToggle mdrawerToggle;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        Parse.initialize(this); // init SDK Parse
+
         setContentView(R.layout.activity_home);
-        /*Button button = (Button)findViewById(R.id.button);
-        button.setOnClickListener(mListenner);*/
+
 
         // Set a Toolbar to replace the ActionBar.
-        toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
+        mToolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(mToolbar);
 
         // Find our drawer view
         mDrawer = (DrawerLayout) findViewById(R.id.drawer_layout);
 
-        // Find our drawer view
-        dlDrawer = (DrawerLayout) findViewById(R.id.drawer_layout);
-        drawerToggle = setupDrawerToggle();
-
         // Tie DrawerLayout events to the ActionBarToggle
-        dlDrawer.setDrawerListener(drawerToggle);
+        mdrawerToggle = setupDrawerToggle();
+        mDrawer.setDrawerListener(mdrawerToggle);
 
         // Set the menu icon instead of the launcher icon.
         final ActionBar ab = getSupportActionBar();
         ab.setHomeAsUpIndicator(R.drawable.ic_menu_white_18dp);
         ab.setDisplayHomeAsUpEnabled(true);
 
-        nvDrawer = (NavigationView) findViewById(R.id.nvView);
         // Setup drawer view
-        setupDrawerContent(nvDrawer);
+        mnvDrawer = (NavigationView) findViewById(R.id.nvView);
+        setupDrawerContent(mnvDrawer);
 
-        
+        // Create a new Fragment to be placed in the activity layout
+        FirstFragment runfragment = new FirstFragment();
 
+        /*// In case this activity was started with special instructions from an
+        // Intent, pass the Intent's extras to the fragment as arguments
+        runfragment.setArguments(getIntent().getExtras());*/
+
+        // Add the fragment to the 'fragment_container' FrameLayout
+        getSupportFragmentManager().beginTransaction().add(R.id.flContent, runfragment)
+                .commit();
     }
 
 
     private ActionBarDrawerToggle setupDrawerToggle() {
-        return new ActionBarDrawerToggle(this, dlDrawer, toolbar, R.string.drawer_open, R.string.drawer_close);
+        return new ActionBarDrawerToggle(this, mDrawer, mToolbar, R.string.drawer_open, R.string.drawer_close);
     }
 
     private void setupDrawerContent(NavigationView navigationView) {
@@ -117,7 +86,7 @@ public class HomeActivity extends AppCompatActivity{
 
     public void selectDrawerItem(MenuItem menuItem) {
 
-        Fragment fragment = null;
+        android.support.v4.app.Fragment fragment = null;
 
         Class fragmentClass;
         switch(menuItem.getItemId()) {
@@ -135,14 +104,14 @@ public class HomeActivity extends AppCompatActivity{
         }
 
         try {
-            fragment = (Fragment) fragmentClass.newInstance();
+            fragment = (android.support.v4.app.Fragment) fragmentClass.newInstance();
         } catch (Exception e) {
             e.printStackTrace();
         }
 
         // Insert the fragment by replacing any existing fragment
-        FragmentManager fragmentManager = getFragmentManager();
-        fragmentManager.beginTransaction()
+        android.support.v4.app.FragmentTransaction fragmenttransaction = getSupportFragmentManager().beginTransaction();
+        fragmenttransaction
                 .replace(R.id.flContent, fragment)
                 .commit();
 
@@ -161,7 +130,7 @@ public class HomeActivity extends AppCompatActivity{
                 mDrawer.openDrawer(GravityCompat.START);
                 return true;
         }
-        if (drawerToggle.onOptionsItemSelected(item)) {
+        if (mdrawerToggle.onOptionsItemSelected(item)) {
             return true;
         }
         return super.onOptionsItemSelected(item);
@@ -171,199 +140,15 @@ public class HomeActivity extends AppCompatActivity{
     protected void onPostCreate(Bundle savedInstanceState) {
         super.onPostCreate(savedInstanceState);
         // Sync the toggle state after onRestoreInstanceState has occurred.
-        drawerToggle.syncState();
+        mdrawerToggle.syncState();
     }
 
     @Override
     public void onConfigurationChanged(Configuration newConfig) {
         super.onConfigurationChanged(newConfig);
         // Pass any configuration change to the drawer toggles
-        drawerToggle.onConfigurationChanged(newConfig);
+        mdrawerToggle.onConfigurationChanged(newConfig);
     }
-
-
-    private View.OnClickListener mListenner = new View.OnClickListener() {
-        @Override
-        public void onClick(View view) {
-            ////////////////////////////////////
-            // Compose Intent camera
-            ////////////////////////////
-            Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
-
-            ///////////////////////////////////////
-            //The Android Camera application saves a full-size photo if you give it a file to save into.
-            // You must provide a fully qualified file name where the camera app should save the photo.
-            ///////////////////
-            fileUri = getOutputMediaFileUri(MEDIA_TYPE_IMAGE); // create a file to save the image
-            intent.putExtra(MediaStore.EXTRA_OUTPUT, fileUri); // set the image file name
-
-            //////////////////////////////////////
-            // small Bitmap in the extras, under the key "data".
-            // Bundle extras = data.getExtras();
-            // Bitmap imageBitmap = (Bitmap) extras.get("data");
-            // mImageView.setImageBitmap(imageBitmap);
-            // ///////////////
-
-            ////////////////////////////////////
-            // Start camera Intent (Implicit MediaStore.ACTION_IMAGE_CAPTURE)
-            ////////////////////////////
-            if(intent.resolveActivity(getPackageManager()) != null){
-                startActivityForResult(intent, CAPTURE_IMAGE_ACTIVITY_REQUEST_CODE);
-            }
-        }
-    };
-            ////////////////////////////////////
-            // Receive a camera Intent Result
-            // A result code specified by the second activity.
-            // This is either RESULT_OK if the operation was successful
-            // or RESULT_CANCELED if the user backed out or the operation failed for some reason.
-            ////////////////////////////
-    @Override
-    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-        if (requestCode == CAPTURE_IMAGE_ACTIVITY_REQUEST_CODE) {
-            if (resultCode == RESULT_OK) {
-                /*Toast.makeText(this, "Image saved to:\n" + fileUri, Toast.LENGTH_LONG).show();*/
-                Log.d("onActivityResult", "result " + fileUri);
-                try{
-
-                    ///////////////////////////////////////
-                    // image to byteArray
-                    // First, you'll need to have the data in byte[] form and then create a ParseFile with it.
-                    ////////////////
-                    Bitmap bitmap = MediaStore.Images.Media.getBitmap(this.getContentResolver(), fileUri);
-                    ByteArrayOutputStream stream = new ByteArrayOutputStream(); //Creates a new byte array output stream. The buffer capacity is initially 32 bytes, though its size increases if necessary.
-                    bitmap.compress(Bitmap.CompressFormat.JPEG, 100, stream);
-                    byte[] byteArray = stream.toByteArray(); // Creates a newly allocated byte array.
-                    stream.close();
-
-                    // URI to String for name file in server
-                    String scheme = fileUri.getScheme();
-                    if (scheme.equals("file")) {
-                        fileName = fileUri.getLastPathSegment();
-                    }
-                    ParseFile file = new ParseFile(fileName, byteArray);
-
-                    // ///////////////////////////////////////
-                    // As with ParseObject, there are many variants of the save method you can use depending
-                    // on what sort of callback and error handling suits you.
-                    ///////////////
-                    file.saveInBackground(new SaveCallback() {
-                        @Override
-                        public void done(com.parse.ParseException e) {
-                              // Handle success or failure here ...
-                            mediaFile.delete();
-                        }
-                    }, new ProgressCallback() {
-
-                          public void done(Integer percentDone) {
-                              // Update your progress spinner here. percentDone will be between 0 and 100.
-                          }
-                    });
-
-                    ///////////////////////////////////////
-                    // Finally, after the save completes, you can associate a
-                    // ParseFile onto a ParseObject just like any other piece of data
-                    ///////////////
-                    ParseObject Parseobject = new ParseObject("LivePic");
-                    Parseobject.put("livepic", file);
-                    /*Parseobject.pin("updateAt", new Date());*/
-                    Parseobject.saveInBackground();
-                }
-                catch(FileNotFoundException e){
-                    e.printStackTrace();
-                }
-                catch(IOException e){
-                    e.printStackTrace();
-                }
-
-            } else if (resultCode == RESULT_CANCELED) {
-                // User cancelled the image capture
-            } else {
-                // Image capture failed, advise user
-                Log.d(TAG, "here");
-            }
-        }
-    }
-
-
-    /** Create a file Uri for saving an image or video */
-    private  Uri getOutputMediaFileUri(int type){
-        return Uri.fromFile(getOutputMediaFile(type));
-    }
-
-    /** Create a File for saving an image or video */
-    private File getOutputMediaFile(int type){
-
-        // To be safe, you should check that the SDCard is mounted
-        // using Environment.getExternalStorageState() before doing this.
-        if(isExternalStorageMounted()){
-
-            // This location works best if you want the created images to be shared
-            // between applications and persist after your app has been uninstalled.
-            File mediaStorageDir = new File(Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_PICTURES), "MyCameraApp");
-
-            // getFilesDir() Returns a File representing an internal directory for your app.
-            // To create a new file in one of these directories, you can use the File() constructor.
-            // internal IS PRIVATE FOR OTHER APP
-            /*Context context = this;
-            File mediaStorageDir = new File(context.getFilesDir(), "LivePic");
-            Log.d(TAG, "result_fileuri=" + fileUri);*/
-
-            // Create the storage directory if it does not exist
-            if (! mediaStorageDir.exists()){
-                if (! mediaStorageDir.mkdirs()){
-                    Log.d("MyCameraApp", "failed to create directory");
-                    return null;
-                }
-            }
-            // Create a media file name
-            String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
-
-            if (type == MEDIA_TYPE_IMAGE){
-                mediaFile = new File(mediaStorageDir.getPath() + File.separator + "IMG_"+ timeStamp + ".jpg");
-            } else {
-                return null;
-            }
-            return mediaFile;
-        }
-        else if(isExternalStorageMountedOncomputer()){
-            ////////////////////////////////////
-            // Error with Environment.getExternalStorageState()
-            // media is connected to computer.
-            // i want block.
-            //////////////////////////
-            Log.e("MountedAtcomputer", "external storage is connected at computer");
-            return null;
-        }
-        else{
-            // Error with Environment.getExternalStorageState()
-            // media is not available.
-            Log.e("StorageMounted", "external storage not available");
-            return null;
-        }
-
-    }
-
-    /** Checks if external storage is available for read and write */
-    private boolean isExternalStorageMounted(){
-        String state = Environment.getExternalStorageState();
-        if(Environment.MEDIA_MOUNTED.equals(state)){
-            return true;
-        }
-        return false;
-    }
-
-    /** Checks if external storage the media is being shared (connected to a computer)
-     *
-     * */
-    private boolean isExternalStorageMountedOncomputer(){
-        String state = Environment.getExternalStorageState();
-        if(Environment.MEDIA_SHARED.equals(state)){
-            return true;
-        }
-        return false;
-    }
-
 
 
 }
